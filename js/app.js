680b35459fe9c29a8dcd3a0aaa68e22fdc6675a2
Laura Escalante <laura088.le@gmail.com>
2024-02-08 17:48:44 -0300

    add try-catch block and css folder

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const $ = (selector) => document.querySelector(selector)
 const $$ = (selector) => document.querySelectorAll(selector)
 
-const urlBase = 'http://gateway.marvel.com/v1/public/'
+const urlBase = 'https://gateway.marvel.com/v1/public/'
 let ts = 'ts=1'
 const publicKey = '&apikey=dbd3f5275340a963c52ebcb09990e187'
 const hash = '&hash=4ff1b9f179fe4dfe2d2b36e2a5fe487c'
@@ -37,9 +37,13 @@ const updateTotalPages = (total) => {
 }
 
 const fetchData = async (url) => {
-    const response = await fetch(url)
-    const data = await response.json()
-    return data.data
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data.data
+    } catch (error) {
+        console.error('Ocurrió un error:', error.message)
+    }
 }
 
 // Parámetros de búsqueda
